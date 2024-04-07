3eed59a7be8593b13e038d22ade5763edd4269fe
Mai Thiên Phú <phumaithien22@gmail.com>
2024-04-07 23:50:17 +0700

    placeholder

--- firebase-react-app/src/firebase.js
+++ firebase-react-app/src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBQ82SWRKrRenzjVoDddQU9cejsbsw1-FY",
+    apiKey: "apiKey",
     authDomain: "ledict-test.firebaseapp.com",
     projectId: "ledict-test",
     storageBucket: "ledict-test.appspot.com",
     messagingSenderId: "895245250742",
-    appId: "1:895245250742:web:6710aa041651ffd1fff617",
-    measurementId: "G-Z9JVDR6KK3"
+    appId: "appId",
+    measurementId: "meaId"
 };
 
 // Initialize Firebase
