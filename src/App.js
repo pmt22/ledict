d3f901cf7e34ea35835fb2fded261b087eda2405
Mai Thiên Phú <phumaithien22@gmail.com>
2024-04-12 23:02:06 +0700

    rename app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {Button} from "@mui/material";
 
-export default function Gallery() {
+export default function App() {
   return (
       <Button variant="contained">Hello world</Button>
   );
