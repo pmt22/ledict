0bc7228165c80c85789059ceea630a79db6d8a2f
Mai Thiên Phú <phumaithien22@gmail.com>
2024-04-14 00:19:42 +0700

    add drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import {Button} from "@mui/material";
+import {Drawer, IconButton, List, ListItem, ListItemButton, ListItemText} from "@mui/material";
+import {useState} from "react";
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import TranslateOutlinedIcon from '@mui/icons-material/TranslateOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 export default function App() {
-  return (
-      <Button variant="contained">Hello world</Button>
-  );
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const list = ['Items', 'Users'];
+
+    return (
+        <>
+            <IconButton onClick={e => setDrawerOpen(true)} children={<MenuOutlinedIcon/>}/>
+            <Drawer
+                anchor="left"
+                open={drawerOpen}
+                onClose={e => setDrawerOpen(false)}
+            >
+                <List>
+                    <ListItem>
+                        <ListItemButton>
+                            <TranslateOutlinedIcon />
+                            <ListItemText primary="Translations" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemButton>
+                            <AccountCircleOutlinedIcon />
+                            <ListItemText primary="Users" />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </Drawer>
+        </>
+    );
 }
